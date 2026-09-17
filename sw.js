8655496a0e66955667157eccb737d96edabf2424
totot222-spec <totot235@gmail.com>
2026-09-17 22:52:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-const CACHE="my-books-v5";
+const CACHE="my-books-v6";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])))});
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{
-  const u=new URL(e.request.url);
-  if(e.request.method==="GET" && (e.request.mode==="navigate" || u.pathname.endsWith("/index.html"))){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-  } else {
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
+ const u=new URL(e.request.url);
+ if(e.request.method==="GET" && (e.request.mode==="navigate" || u.pathname.endsWith("/index.html"))){
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+ } else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
