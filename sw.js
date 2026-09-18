0f2afbf1ba5da2f4acc1e9fc05bbce86598e6cf9
totot222-spec <totot235@gmail.com>
2026-09-18 10:15:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE="my-books-v1.0";
+constconst CACHE="my-books-v1.2";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
