d2b7fc44d05901dd749717c7bdd2a15d65f3040b
totot222-spec <totot235@gmail.com>
2026-09-18 13:58:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-const CACHE="my-books-v1.1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="my-books-v2";
+const ASSETS=["./","./index.html","./manifest.json"];
+
+self.addEventListener("install",e=>{
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener("activate",e=>{
+ e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+ );
+ self.clients.claim();
+});
+
+// ネットワークを優先し、取得できた最新版を常にキャッシュへ保存。
+// オフライン時だけキャッシュ済みの内容にフォールバックする。
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  fetch(e.request).then(res=>{
+   const copy=res.clone();
+   caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+   return res;
+  }).catch(()=>caches.match(e.request))
+ );
+});
